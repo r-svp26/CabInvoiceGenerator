86ef48ff192638525816b902e4f54acd164bd378
r-svp26 <riteshk6341@gmail.com>
2021-08-26 14:37:18 +0530

    [Ritesh] Add: Welcome to Cab Service Portal

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    /// <summary>
+    /// main class
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator");
+        }
+    }
+}
